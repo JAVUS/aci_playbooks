21f7386580c07b44abb4e96dd061d72567a96950
Javier Utrilla <66509137+JAVUS@users.noreply.github.com>
2026-02-17 14:56:53 -0500

    Configure Terraform provider for Cisco ACI

--- provider.tf
+++ provider.tf
@@ -1,6 +1,22 @@
+terraform {
+  required_providers {
+    aci = {
+      source  = "CiscoDevNet/aci"
+      version = ">= 2.10.0" # Es buena práctica fijar una versión
+    }
+  }
+
+  cloud {
+    organization = "cisco_aci"
+    workspaces {
+      name = "terraform_cloud"
+    }
+  }
+}
+
 provider "aci" {
   username = "admin"
   password = var.apic_password
   url      = "https://38.25.68.158:7443"
-  insecure = true # Normalmente necesario si usas certificados auto-firmados
+  insecure = true
 }
